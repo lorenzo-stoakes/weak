c3fa33a23134738b4d609b77e13c86b68dfaef0e
Lorenzo Stoakes <lstoakes@gmail.com>
2012-06-24 13:40:05 +0100

    Add pawnLegal logic.

--- game.c
+++ game.c
@@ -99,10 +99,105 @@ castleLegal(Game *game, bool queenSide)
   return false;
 }
 
+// Is this pawn move legal?
 static bool
 pawnLegal(Game *game, Move *move)
 {
-  return false;
+  BitBoard expectedFromBoard, lastToBoard, sources, targets, validToRankMask;
+  BitBoard fromBoard = POSBOARD(move->From), toBoard = POSBOARD(move->To);
+  int dist;
+  MoveSlice hist;
+  Move lastMove;
+  Position expectedTo, lastFrom, lastTo;
+  Rank expectedRank, offset, rank;
+
+  switch(move->Type) {
+  default:
+    panic("Move type %d in pawnLegal.", move->Type);
+  case EnPassant:
+    // En passants are captures. Full stop.
+    if(!move->Capture) {
+      return false;
+    }
+
+    // We have to check the last move to see whether en passant is valid.
+    hist = game->History.Moves;
+    if(hist.Len == 0) {
+      return false;
+    }
+    lastMove = hist.Vals[hist.Len-1];
+    lastFrom = lastMove.From;
+    lastTo = lastMove.To;
+    if(lastMove.Piece != Pawn) {
+      return false;
+    }
+    dist = RANK(lastFrom) - RANK(lastTo);
+    if(dist != 2 && dist != -2) {
+      return false;
+    }
+
+    // En passant has to originate from either side of where the pawn has now moved.
+    lastToBoard = POSBOARD(lastTo);
+    expectedFromBoard = WestOne(lastToBoard) | EastOne(lastToBoard);
+    if((fromBoard&expectedFromBoard) != fromBoard) {
+      return false;
+    }
+
+    // Have to capture into square immediately behind just moved opposing pawn.
+    if(game->WhosTurn == White) {
+      offset = -1;
+    } else if(game->WhosTurn == Black) {
+      offset = 1;
+    } else {
+      panic("Unrecognised side %d.", game->WhosTurn);
+    }
+    expectedTo = POSITION(RANK(lastFrom)+offset, FILE(lastFrom));
+    if(move->To != expectedTo) {
+      return false;
+    }
+
+    return true;
+  case PromoteKnight:
+  case PromoteBishop:
+  case PromoteRook:
+  case PromoteQueen:
+    rank = RANK(move->To);
+    if(game->WhosTurn == White) {
+      expectedRank = Rank8;
+      validToRankMask = Rank8Mask;
+    } else if(game->WhosTurn == Black) {
+      expectedRank = Rank1;
+      validToRankMask = Rank1Mask;
+    } else {
+      panic("Unrecognised side %d.", game->WhosTurn);
+    }
+  if(rank != expectedRank) {
+    return false;
+  }
+  break;
+  case Normal:
+    if(game->WhosTurn == White) {
+      validToRankMask = NotRank8Mask;
+    } else if(game->WhosTurn == Black) {
+      validToRankMask = NotRank1Mask;
+    } else {
+      panic("Unrecognised side %d.", game->WhosTurn);
+    }
+    break;
+  }
+
+  if(move->Capture) {
+    sources = PawnCaptureSources(game->WhosTurn, &game->ChessSet, fromBoard);
+    targets = PawnCaptureTargets(game->WhosTurn, &game->ChessSet, fromBoard);
+  } else {
+    sources = PawnPushSources(game->WhosTurn, &game->ChessSet, fromBoard);
+    targets = PawnPushTargets(game->WhosTurn, &game->ChessSet, fromBoard);
+  }
+
+  // Since sources are referenced against our single candidate pawn, their being non-empty
+  // means the pawn is able to move. If the pawn's valid targets and our 'to' position
+  // intersect, then the to position is valid.
+  return sources != EmptyBoard && (targets&toBoard&validToRankMask) != EmptyBoard;
 }
 
 static bool rookLegal(Game *game, Move *move)
