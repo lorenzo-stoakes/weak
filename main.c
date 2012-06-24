df05c556ada816c3b4c34865b94a3e6466522232
Lorenzo Stoakes <lstoakes@gmail.com>
2012-06-24 16:44:27 +0100

    Output all moves from initial position.

--- main.c
+++ main.c
@@ -4,7 +4,20 @@
 int
 main(int argc, char **argv)
 {
+  int i;
+  Game game;
+  Move move;
+  MoveSlice moves;
+
   puts("WeakC v0.0.dev.\n");
+
+  game = NewGame(true, White);
+  moves = AllMoves(&game);
+
+  for(i = 0; i < moves.Len; i++) {
+    move = moves.Vals[i];
+    puts(StringMove(&move));
+  }
 
   return 0;
 }
