fab0d6a72579d99ec9d892857347c35597cf9688
Lorenzo Stoakes <lstoakes@gmail.com>
2012-06-24 16:14:52 +0100

    Moving castle event logic into separate file.

---
+++ castleevent.c
@@ -0,0 +1,47 @@
+#include <string.h>
+#include "weak.h"
+
+static const int INIT_CASTLE_EVENT_COUNT = 4;
+
+// Append CastleEvent to specified CastleEvent slice.
+CastleEventSlice
+AppendCastleEvent(CastleEventSlice slice, CastleEvent event)
+{
+  // TODO: Avoid duplication :-)  
+
+  CastleEventSlice ret;
+
+  if(slice.Len > slice.Cap) {
+    panic("CastleEventSlice.Len %d > CastleEventSlice.Cap %d - Impossible!", slice.Len,
+          slice.Cap);
+  }
+
+  // Expand.
+  if(slice.Len == slice.Cap) {
+    ret.Cap = slice.Cap*2;
+    ret.Vals = (CastleEvent*)allocate(sizeof(CastleEvent)*ret.Cap);
+    memcpy(ret.Vals, slice.Vals, slice.Len);
+    release(slice.Vals);
+  } else {
+    ret.Vals = slice.Vals;
+    ret.Cap = slice.Cap;
+  }
+
+  ret.Vals[slice.Len] = event;
+  ret.Len = slice.Len+1;
+
+  return ret;
+}
+
+CastleEventSlice
+NewCastleEventSlice()
+{
+  CastleEventSlice ret;
+
+  ret.Len = 0;
+  ret.Cap = INIT_CASTLE_EVENT_COUNT;
+  ret.Vals = (CastleEvent*)allocate(sizeof(CastleEvent)*INIT_CASTLE_EVENT_COUNT);
+
+  return ret;
+}
+
