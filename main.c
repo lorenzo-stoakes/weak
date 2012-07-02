861799ab3e8b68f9676366eb105f3ec1c4996c1f
Lorenzo Stoakes <lstoakes@gmail.com>
2012-07-02 21:21:02 +0100

    Time perft.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <sys/time.h>
 #include "weak.h"
 
 // Initialise lookup tables, etc.
@@ -12,17 +13,30 @@ init()
 int
 main(int argc, char **argv)
 {
+  double elapsed;
   Game game;
   PerftStats stats;
+  struct timeval start, end;
 
   init();
+  puts("WeakC v0.0.dev.\n");  
 
-  puts("WeakC v0.0.dev.\n");
+  printf("Initialising... ");
+  init();
+  puts("done.\n");
 
   game = NewGame(true, White);
 
+  gettimeofday(&start, NULL);
   stats = Perft(&game, 4);
+  gettimeofday(&end, NULL);
+
+  elapsed = (end.tv_sec - start.tv_sec) * 1000.0;
+  elapsed += (end.tv_usec - start.tv_usec) / 1000.0;
+
   puts(StringPerft(&stats));
+
+  printf("%f ms elapsed.\n", elapsed);
 
   return 0;
 }
