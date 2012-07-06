bee8704750ce76fd0a69bae0a9c928d7112f0c17
Lorenzo Stoakes <lstoakes@gmail.com>
2012-07-06 02:22:06 +0100

    Do all rook operations via threats and use magic bitboards.

--- rook.c
+++ rook.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
 #include "weak.h"
+#include "magic.h"
+
+static FORCE_INLINE BitBoard
+magicSquareThreats(Position rook, BitBoard occupancy) {
+  BitBoard magic = magicBoard[MAGIC_ROOK][rook];
+  BitBoard mask = magicMask[MAGIC_ROOK][rook];
+  int shift = magicShift[MAGIC_ROOK][rook];
+  BitBoard index = (magic*(occupancy&mask))>>shift;
+
+  // TODO: Include non-inner 6 bit positions!
+
+  return RookThreatBase[rook][index];
+}
 
 // Get BitBoard encoding capture targets for *all* rooks on specified side.
 BitBoard
@@ -34,17 +48,37 @@ AllRookMoveTargets(ChessSet *chessSet, Side side)
 BitBoard
 AllRookThreats(ChessSet *chessSet, Side side)
 {
-  return AllRookMoveTargets(chessSet, side) | AllRookCaptureTargets(chessSet, side);
+  BitBoard ret, rooks;
+  Position rook;
+
+  switch(side) {
+  case White:
+    rooks = chessSet->White.Rooks;
+    break;
+  case Black:
+    rooks = chessSet->Black.Rooks;
+    break;
+  default:
+    panic("Unrecognised side %d.", side);
+  }
+
+  ret = EmptyBoard;
+  for(; rooks; rooks ^= POSBOARD(rook)) {
+    rook = BitScanForward(rooks);
+
+    ret |= magicSquareThreats(rook, chessSet->Occupancy);
+  }
+
+  return ret;
 }
 
-// Get BitBoard encoding capture targets for specified rooks.
+// Get BitBoard encoding capture targets for specified rooks without using magic BitBoards.
 BitBoard
 RookCaptureTargets(ChessSet *chessSet, Side side, BitBoard rooks)
 {
-  BitBoard blockers, east, nort, sout, west, occupancy, opposition, ret;
-  Position blocker, rook;
-
-  occupancy = chessSet->Occupancy;
+  BitBoard opposition;
+  BitBoard ret = EmptyBoard;
+  Position rook;
 
   switch(side) {
   case White:
@@ -62,85 +96,67 @@ RookCaptureTargets(ChessSet *chessSet, Side side, BitBoard rooks)
   for(; rooks; rooks ^= POSBOARD(rook)) {
     rook = BitScanForward(rooks);
 
-    nort = NortRay(rook);
-    blockers = nort & occupancy;
-    if(blockers != EmptyBoard) {
-      blocker = BitScanForward(blockers);
-      ret |= POSBOARD(blocker);
-    }
-
-    east = EastRay(rook);
-    blockers = east & occupancy;
-    if(blockers != EmptyBoard) {
-      blocker = BitScanForward(blockers);
-      ret |= POSBOARD(blocker);
-    }
-
-    sout = SoutRay(rook);
-    blockers = sout & occupancy;
-    if(blockers != EmptyBoard) {
-      blocker = BitScanBackward(blockers);
-      ret |= POSBOARD(blocker);
-    }
-
-    west = WestRay(rook);
-    blockers = west & occupancy;
-    if(blockers != EmptyBoard) {
-      blocker = BitScanBackward(blockers);
-      ret |= POSBOARD(blocker);
-    }
+    ret |= magicSquareThreats(rook, chessSet->Occupancy);
   }
 
   return ret & opposition;
 }
 
-// Get BitBoard encoding move targets for specified rooks.
+// Get BitBoard encoding move targets for specified rooks without using magic BitBoards.
 BitBoard
 RookMoveTargets(ChessSet *chessSet, Side side, BitBoard rooks)
 {
-  BitBoard blockers, east, nort, sout, west, occupancy, ret;
-  Position blocker, rook;
-
-  occupancy = ChessSetOccupancy(chessSet);
-  occupancy = chessSet->Occupancy;
-
-  ret = EmptyBoard;
+  BitBoard ret = EmptyBoard;
+  Position rook;
 
   for(; rooks; rooks ^= POSBOARD(rook)) {
     rook = BitScanForward(rooks);
 
-    nort = NortRay(rook);
-    blockers = nort & occupancy;
-    if(blockers != EmptyBoard) {
-      blocker = BitScanForward(blockers);
-      nort ^= blockers | NortRay(blocker);
-    }
-    ret |= nort;
-
-    east = EastRay(rook);
-    blockers = east & occupancy;
-    if(blockers != EmptyBoard) {
-      blocker = BitScanForward(blockers);
-      east ^= blockers | EastRay(blocker);
-    }
-    ret |= east;
-
-    sout = SoutRay(rook);
-    blockers = sout & occupancy;
-    if(blockers != EmptyBoard) {
-      blocker = BitScanBackward(blockers);
-      sout ^= blockers | SoutRay(blocker);
-    }
-    ret |= sout;
-
-    west = WestRay(rook);
-    blockers = west & occupancy;
-    if(blockers != EmptyBoard) {
-      blocker = BitScanBackward(blockers);
-      west ^= blockers | WestRay(blocker);
-    }
-    ret |= west;
+    ret |= magicSquareThreats(rook, chessSet->Occupancy);
   }
+
+  return ret & chessSet->EmptySquares;
+}
+
+// Get rook threats from the specified square and occupancy.
+BitBoard
+RookSquareThreats(Position rook, BitBoard occupancy)
+{
+  BitBoard blockers, east, nort, sout, west;
+  BitBoard ret = EmptyBoard;
+  Position blocker;
+
+  nort = NortRay(rook);
+  blockers = nort & occupancy;
+  if(blockers != EmptyBoard) {
+    blocker = BitScanForward(blockers);
+    nort &= ~NortRay(blocker);
+  }
+  ret |= nort;
+
+  east = EastRay(rook);
+  blockers = east & occupancy;
+  if(blockers != EmptyBoard) {
+    blocker = BitScanForward(blockers);
+    east &= ~EastRay(blocker);
+  }
+  ret |= east;
+
+  sout = SoutRay(rook);
+  blockers = sout & occupancy;
+  if(blockers != EmptyBoard) {
+    blocker = BitScanBackward(blockers);
+    sout &= ~SoutRay(blocker);
+  }
+  ret |= sout;
+
+  west = WestRay(rook);
+  blockers = west & occupancy;
+  if(blockers != EmptyBoard) {
+    blocker = BitScanBackward(blockers);
+    west &= ~WestRay(blocker);
+  }
+  ret |= west;
 
   return ret;
 }
