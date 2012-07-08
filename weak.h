2a1254df0212634d22208b5e79bce61d609041d7
Lorenzo Stoakes <lstoakes@gmail.com>
2012-07-08 12:18:34 +0100

    Add parens in macros to prevent irritating issues with unexpected results!

--- weak.h
+++ weak.h
@@ -10,11 +10,11 @@
 
 // See http://chessprogramming.wikispaces.com/Bitboards.
 #define C64(constantU64) constantU64##ULL
-#define RANK(pos) ((Rank)(pos/8))
-#define FILE(pos) ((File)(pos&7))
-#define OPPOSITE(side) ((Side)(1-side))
-#define POSBOARD(pos) ((BitBoard)(1ULL<<pos))
-#define POSITION(rank, file) ((Position)(rank*8 + file))
+#define RANK(pos) ((Rank)((pos)/8))
+#define FILE(pos) ((File)((pos)&7))
+#define OPPOSITE(side) ((Side)(1-(side)))
+#define POSBOARD(pos) ((BitBoard)(1ULL<<(pos)))
+#define POSITION(rank, file) ((Position)((rank)*8 + (file)))
 
 #define FORCE_INLINE inline __attribute__((always_inline))
 
