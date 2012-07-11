a93cc55d9506bbacdb5b7ade0010f911d6022723
Lorenzo Stoakes <lstoakes@gmail.com>
2012-07-11 19:43:32 +0100

    Improve readability of benchmark output.

--- benches/util.c
+++ benches/util.c
@@ -5,11 +5,11 @@
 void
 OutputBenchResults(char *name, double elapsed, long iters, int64_t nodes)
 {
-  printf("%s: %ld %f ms/op", name, iters, elapsed/iters);
+  printf("%s:\t%ld\t%.3f\tms/op", name, iters, elapsed/iters);
 
   // Not all benchmarks will necessarily return node count, if not they each return -1.
   if(nodes > 0) {
-    printf(" %f n/s", 1000*nodes/elapsed);
+    printf("\t%.3f\tn/s", 1000*nodes/elapsed);
   }
 
   printf("\n");
