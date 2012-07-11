da4d51436dbf8428b5c4b95491b37e168024b030
Lorenzo Stoakes <lstoakes@gmail.com>
2012-07-11 19:05:30 +0100

    Move perft positions into weak.h

--- weak.h
+++ weak.h
@@ -20,6 +20,16 @@
 
 #define INIT_MOVE_LEN 100
 
+// Perft positions, see http://chessprogramming.wikispaces.com/Perft+Results.
+
+#define PERFT_COUNT 5
+
+#define FEN1 "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+#define FEN2 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -"
+#define FEN3 "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -"
+#define FEN4 "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
+#define FEN4_REVERSED "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1"
+
 enum CastleEvent {
   NoCastleEvent = 0,
   LostKingSideWhite = 1 << 0,
