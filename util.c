8eff0d8937c2d446267a49f2935b9aa0ae8189a3
Lorenzo Stoakes <lstoakes@gmail.com>
2012-07-17 22:03:56 +0100

    builder doesnt deserve its own file :)

--- util.c
+++ util.c
@@ -6,6 +6,10 @@
 
 // We violate naming convention here for familiarity-with-go's sake. :-) TODO: Fix.
 
+#define INIT_BUILDER_SIZE 10
+
+static void expandBuilder(StringBuilder*);
+
 // Wrapper for malloc. We might change the implementation later.
 void*
 allocate(size_t size, size_t num)
@@ -46,10 +50,102 @@ release(void *ptr)
   free(ptr);
 }
 
+void
+AppendString(StringBuilder *builder, char *str)
+{
+  int strLen = strlen(str);
+
+  if(strLen <= 0) {
+    return;
+  }
+
+  if(builder->cap < builder->len) {
+    panic("Impossible.");
+  }
+  if(builder->cap == builder->len) {
+    expandBuilder
+(builder);
+  }
+
+  builder->strings[builder->len] = strdup(str);
+  builder->len++;
+
+  builder->Length += strLen;
+}
+
+char*
+BuildString(StringBuilder *builder, bool releaseBuilder)
+{
+  char *ret;
+  int i, j, k;
+
+  char *str;
+
+  if(builder->Length <= 0) {
+    return NULL;
+  }
+
+  ret = (char*)allocate(sizeof(char), builder->Length + 1);
+
+  k = 0;
+  for(i = 0; i < builder->len && k < builder->Length; i++) {
+    str = builder->strings[i];
+    for(j = 0; str[j] != '\0'; j++) {
+      ret[k] = str[j];
+      k++;
+    }
+  }
+
+  ret[k] = '\0';
+
+  if(releaseBuilder) {
+    ReleaseStringBuilder(builder);
+  }
+
+  return ret;
+}
+
+StringBuilder
+NewStringBuilder()
+{
+  StringBuilder ret;
+
+  ret.Length = 0;
+
+  ret.cap = INIT_BUILDER_SIZE;
+  ret.len = 0;
+  ret.strings = (char**)allocate(sizeof(char*), INIT_BUILDER_SIZE);
+
+  return ret;
+}
+
+void
+ReleaseStringBuilder(StringBuilder *builder)
+{
+  int i;
+
+  for(i = 0; i < builder->len; i++) {
+    release(builder->strings[i]);
+  }
+}
+
 // Set stdout output unbuffered.
 void
 SetUnbufferedOutput()
 {
   // Use unbuffered output.
   setbuf(stdout, NULL);
+}
+
+
+static void
+expandBuilder(StringBuilder *builder)
+{
+  char **buffer;
+
+  builder->cap *= 2;
+  buffer = (char**)allocate(sizeof(char*), builder->cap);
+  memcpy(buffer, builder->strings, builder->len*sizeof(char*));
+  release(builder->strings);
+  builder->strings = buffer;
 }
