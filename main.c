6256a7870c0daf1a68cac31a1e830090309b1942
Lorenzo Stoakes <lstoakes@gmail.com>
2012-07-22 19:37:32 +0100

    Make main do something vaguely interesting for now :)

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <time.h>
 #include "weak.h"
 
 int
 main(int argc, char **argv)
 {
+  clock_t ticks;
+  double elapsed;
+  Game game;
+  uint64_t ret;
+
   SetUnbufferedOutput();
 
   puts("WeakC v0.0.dev.\n");
@@ -12,7 +18,15 @@ main(int argc, char **argv)
   InitEngine();
   puts("done.\n");
 
-  puts("Work in progress :-) Try make bench for some output.");
+  game = ParseFen(FEN1);
+
+  ticks = clock();
+  ret = QuickPerft(&game, 4);
+  ticks = clock() - ticks;
+  // In ms.
+  elapsed = 1000*((double)ticks)/CLOCKS_PER_SEC;
+
+  printf("%llu - %f elapsed.\n", ret, elapsed);
 
   return 0;
 }
