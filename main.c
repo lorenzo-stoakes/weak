838c96ec2e7dc22571c8620e39531e88e9d4d1ce
Lorenzo Stoakes <lstoakes@gmail.com>
2012-07-24 22:42:02 +0100

    Make main.c do something more useful/interesting.

--- main.c
+++ main.c
@@ -2,13 +2,23 @@
 #include <time.h>
 #include "weak.h"
 
+//#define SHOW_PERFT_STATS
+
+#define FEN FEN1
+#define DEPTH 4
+
 int
 main(int argc, char **argv)
 {
   clock_t ticks;
   double elapsed;
   Game game;
+
+#if defined(SHOW_PERFT_STATS)
+  PerftStats ret;
+#else
   uint64_t ret;
+#endif
 
   SetUnbufferedOutput();
 
@@ -18,15 +28,24 @@ main(int argc, char **argv)
   InitEngine();
   puts("done.\n");
 
-  game = ParseFen(FEN1);
+  game = ParseFen(FEN);
 
   ticks = clock();
-  ret = QuickPerft(&game, 4);
+#if defined(SHOW_PERFT_STATS)
+  ret = Perft(&game, DEPTH);
+#else
+  ret = QuickPerft(&game, DEPTH);
+#endif
   ticks = clock() - ticks;
   // In ms.
   elapsed = 1000*((double)ticks)/CLOCKS_PER_SEC;
 
+#if defined(SHOW_PERFT_STATS)
+  puts(StringPerft(&ret));
+  printf("%f elapsed.\n", elapsed);
+#else
   printf("%llu - %f elapsed.\n", ret, elapsed);
+#endif
 
   return 0;
 }
