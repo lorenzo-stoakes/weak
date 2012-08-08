a6bf6081934a2f1ec6fd2e2261ca4f4620076321
Lorenzo Stoakes <lstoakes@gmail.com>
2012-08-08 19:31:31 +0100

    Remove repetitions and add potentially cheeky warm up.

--- main.c
+++ main.c
@@ -6,12 +6,10 @@
 
 #define FEN FEN1
 #define DEPTH 6
-#define REPETITIONS 1
 
 int
 main()
 {
-  int i;
   clock_t ticks;
   double elapsed;
   Game game;
@@ -34,17 +32,25 @@ main()
 
   puts(StringChessSet(&game.ChessSet));
 
-  ticks = clock();
-  for(i = 0; i < REPETITIONS; i++) {
-#if defined(SHOW_PERFT_STATS)
-    ret = Perft(&game, DEPTH);
-#else
-    ret = QuickPerft(&game, DEPTH);
-#endif
+  // Warm up.
+  if(DEPTH > 2) {
+    QuickPerft(&game, DEPTH-2);
+    QuickPerft(&game, DEPTH-1);    
+    QuickPerft(&game, DEPTH);    
   }
+
+  ticks = clock();
+
+#if defined(SHOW_PERFT_STATS)
+  ret = Perft(&game, DEPTH);
+#else
+  ret = QuickPerft(&game, DEPTH);
+#endif
+    
   ticks = clock() - ticks;
+
   // In ms.
-  elapsed = 1000*((double)ticks)/CLOCKS_PER_SEC/REPETITIONS;
+  elapsed = 1000*((double)ticks)/CLOCKS_PER_SEC;
 
 #if defined(SHOW_PERFT_STATS)
   puts(StringPerft(&ret));
