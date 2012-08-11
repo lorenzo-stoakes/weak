7cb4e93ef70a3e20069ea70f108fe9d54dcd5b66
Lorenzo Stoakes <lstoakes@gmail.com>
2012-08-11 13:56:09 +0100

    Implement minimax with alpha/beta pruning.

--- search.c
+++ search.c
@@ -21,17 +21,102 @@
 
 #include "weak.h"
 
-Move
-Search(Game *game)
+#define DEPTH 6
+
+#define SMALL -1E10
+#define BIG    1E10
+
+// Perform hacky mini-max. Note the scoring is from white's perspective.
+static double
+miniMax(Game *game, double alpha, double beta, int depth, uint64_t *count)
 {
-  int len;
+  double max, val;
   Move moves[INIT_MOVE_LEN];
-  Move *start =moves, *end;
+  Move *start = moves;
+  Move *curr, *end;
+  Side side = game->WhosTurn;
+
+  if(depth == DEPTH) {
+    return Eval(game);
+  }
 
   end = AllMoves(moves, game);
-  len = end - start;
 
-  // Choose random move.
+  *count += end-start;
 
-  return moves[rand() % len];
+  // Iterate through all moves looking for the best, whose definition
+  // varies based on who's turn it is.
+
+  max = side == White ? SMALL : BIG;
+
+  for(curr = start; curr != end; curr++) {
+    DoMove(game, *curr);
+    val = miniMax(game, alpha, beta, depth+1, count);
+    Unmove(game);
+
+    if(side == White) {
+      if(val > beta) {
+        return val;
+      }      
+
+      if(val > max) {
+        max = val;
+        alpha = val;
+      }
+    } else {
+      if(val < alpha) {
+        return val;
+      }
+
+      if(val < max) {
+        max = val;
+        beta = val;
+      }
+    }
+  }
+
+  return max;
+}
+
+// Perform hacky mini-max. Note the scoring is from white's perspective.
+Move
+Search(Game *game, uint64_t *count)
+{
+  double max, val;
+  Move moves[INIT_MOVE_LEN];
+  Move best;
+  Move *start = moves;
+  Move *curr, *end;
+  Side side = game->WhosTurn;
+
+  // Don't perform alpha/beta pruning for now.
+
+  end = AllMoves(moves, game);
+
+  *count = end-start;
+
+  // Iterate through all moves looking for the best, whose definition
+  // varies based on who's turn it is.
+
+  max = side == White ? SMALL : BIG;
+  best = INVALID_MOVE;
+
+  for(curr = start; curr != end; curr++) {
+    DoMove(game, *curr);
+
+    val = miniMax(game, SMALL, BIG, 1, count);
+
+    if((side == White && val > max) || (side == Black && val < max)) {
+      max = val;
+      best = *curr;
+    }
+
+    Unmove(game);
+  }
+
+  if(best == INVALID_MOVE) {
+    panic("No move selected!");
+  }
+
+  return best;
 }
