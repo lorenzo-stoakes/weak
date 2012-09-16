78973695d3a84991342c747de51c2123c02f605e
Lorenzo Stoakes <lstoakes@gmail.com>
2012-09-16 23:16:00 +0100

    Not quite identical :-)

--- trans.c
+++ trans.c
@@ -19,7 +19,7 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// Derived from Stockfish transposition table. Pretty much identical :-)
+// Derived from Stockfish transposition table.
 
 #include "weak.h"
 
